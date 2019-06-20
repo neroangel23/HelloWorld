d6e2dce7613e73e0e69fd8f03fedd6f8a1cf9e79
neroangel23 <36700925+neroangel23@users.noreply.github.com>
2019-06-20 09:52:05 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title></title>
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <?php
+            $num1 = 123;
+            $var1 = 'Hello';
+            $var2 = 'World';
+            echo $var1 . " " . $var2 ." ". $num1;
+        ?>
+        <script>
+
+        </script>    
+    </body>
+</html>
